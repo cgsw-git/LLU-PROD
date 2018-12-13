6512cbe987d921b0498e480180724392334f6965
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-13 14:46:59 -0800

    Update INCLUDES_CUSTOM.js

--- Scripts/INCLUDES_CUSTOM.js
+++ Scripts/INCLUDES_CUSTOM.js
@@ -1439,3 +1439,55 @@ function sendNotification(emailFrom,emailTo,emailCC,templateName,params,reportFi
 /*------------------------------------------------------------------------------------------------------/
 |  Notification Template Functions (End)
 /------------------------------------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------
+function updateRowsFromASIT
+-------------------------------------------------------------*/
+
+/**
+* Set update column value. format: Map<rowID, Map<columnName, columnValue>>
+**/
+function setUpdateColumnValue(updateRowsMap/** Map<rowID, Map<columnName, columnValue>> **/, rowID, columnName, columnValue)
+{
+	var updateFieldsMap = updateRowsMap.get(rowID);
+	if (updateFieldsMap == null)
+	{
+		updateFieldsMap = aa.util.newHashMap();
+		updateRowsMap.put(rowID, updateFieldsMap);
+	}
+	updateFieldsMap.put(columnName, columnValue);
+}
+
+/**
+* update ASIT rows data. updateRowsMap format: Map<rowID, Map<columnName, columnValue>>
+**/
+function updateAppSpecificTableInfors(tableName, capIDModel, updateRowsMap/** Map<rowID, Map<columnName, columnValue>> **/)
+{
+	if (updateRowsMap == null || updateRowsMap.isEmpty())
+	{
+		return;
+	}
+	
+	var asitTableScriptModel = aa.appSpecificTableScript.createTableScriptModel();
+	var asitTableModel = asitTableScriptModel.getTabelModel();
+	var rowList = asitTableModel.getRows();
+	asitTableModel.setSubGroup(tableName);
+	var rowIdArray = updateRowsMap.keySet().toArray();
+	for (var i = 0; i < rowIdArray.length; i++)
+	{
+		var rowScriptModel = aa.appSpecificTableScript.createRowScriptModel();
+		var rowModel = rowScriptModel.getRow();
+		rowModel.setFields(updateRowsMap.get(rowIdArray[i]));
+		rowModel.setId(rowIdArray[i]);
+		rowList.add(rowModel);
+	}
+	return aa.appSpecificTableScript.updateAppSpecificTableInfors(capIDModel, asitTableModel);
+}
+
+/*-------------------------------------------------------------
+function updateRowsFromASIT end
+-------------------------------------------------------------*/
+
+
+
