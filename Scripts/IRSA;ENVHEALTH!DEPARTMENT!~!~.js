92b51dd007ff790e379d913f9f2a592fbee5b429
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-18 21:37:03 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -9,8 +9,43 @@ var myResult = aa.cap.getCap(inspObj.getCapID());
 if (myResult.getSuccess()){
   cap = myResult.getOutput();
   capId = cap.getCapID();
+  logDebugObject(capId);
+  logDebug("customID = " + capId.getCustomID());
   runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
 } else {
   logDebug("ERROR: Could not get CapId : " + myResult.getErrorType() + " : " + myResult.getErrorMessage() );
 }
 // End generate and attach the 5001 Inspection Report to the inspected record
+
+function logDebugObject(myObject) {
+/*
+usage - logDebugObject(object)
+
+author - Michael Zachry
+created - 10/10/2018
+
+updates
+10/11/2018 - initial version
+
+*/
+  //list the methods
+  try {
+    logDebug("object is is a " + myObject.getClass());
+    logDebug("object has the following methods:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) == "function" ) {
+        logDebug("  " + x);
+      }
+    }
+
+    //list the properties and values    
+    logDebug("object has the following properties and values:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) != "function" ) {
+        logDebug("  " + x + " = " + myObject[x]);
+      }
+    }
+  } catch (err) {
+    logDebug("A JavaScript Error occured: " + err.message);
+  }
+}
