ad040b788589453a2fb8837026aaeecf63391a8b
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-18 22:27:11 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -1,18 +1,18 @@
 showDebug = true; 
 showMessage = true; 
-var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
-var inspType = inspObj.getInspectionType();
+// var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
+// var inspType = inspObj.getInspectionType();
 logDebug("Inspection Date= " + inspDate);
 
 //generate and attach the 5001 Inspection Report to the inspected record
-var myResult = aa.cap.getCap(inspObj.getCapID());
-if (myResult.getSuccess()){
-  var cap = myResult.getOutput();
-  var capId = cap.getCapID();
-  var customID = capId.getCustomID();
-  runReportAttach(capId,"5001 Inspection Report","departmentID",customID,"inspectionDate",inspDate,"inspectionType",inspType);
-} else {
-  logDebug("ERROR: Could not get CapId : " + myResult.getErrorType() + " : " + myResult.getErrorMessage() );
-}
+// var myResult = aa.cap.getCap(inspObj.getCapID());
+// if (myResult.getSuccess()){
+  // var cap = myResult.getOutput();
+  // var capId = cap.getCapID();
+  // var customID = capId.getCustomID();
+  // runReportAttach(capId,"5001 Inspection Report","departmentID",customID,"inspectionDate",inspDate,"inspectionType",inspType);
+// } else {
+  // logDebug("ERROR: Could not get CapId : " + myResult.getErrorType() + " : " + myResult.getErrorMessage() );
+// }
 // End generate and attach the 5001 Inspection Report to the inspected record
 
