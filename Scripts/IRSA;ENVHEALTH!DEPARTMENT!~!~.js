1aaa4f46ef63ae71b1f2227e88e219c63bc101f4
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-18 19:40:53 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -2,43 +2,8 @@ showDebug = true;
 showMessage = true; 
 var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
 var inspType = inspObj.getInspectionType();
-var capId = inspObj.getCapID();
+var capId = capId.getCapID();
 logDebug("Inspection Date= " + inspDate);
 
 //generate and attach the 5001 Inspection Report to the inspected record
-//runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
-logDebugObject(capId);
-
-function logDebugObject(myObject) {
-/*
-usage - logDebugObject(object)
-
-author - Michael Zachry
-created - 10/10/2018
-
-updates
-10/11/2018 - initial version
-
-*/
-  //list the methods
-  try {
-    logDebug("object is is a " + myObject.getClass());
-    logDebug("object has the following methods:");
-    for (x in myObject) {
-      if (typeof(myObject[x]) == "function" ) {
-        logDebug("  " + x);
-      }
-    }
-
-    //list the properties and values    
-    logDebug("object has the following properties and values:");
-    for (x in myObject) {
-      if (typeof(myObject[x]) != "function" ) {
-        logDebug("  " + x + " = " + myObject[x]);
-      }
-    }
-  } catch (err) {
-    logDebug("A JavaScript Error occured: " + err.message);
-  }
-}
-
+runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
