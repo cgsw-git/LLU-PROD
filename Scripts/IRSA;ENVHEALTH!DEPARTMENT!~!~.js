119a49ad2f011b8172f68c415615d81e218d05b2
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-18 17:11:22 -0800

    Create IRSA;ENVHEALTH!DEPARTMENT!~!~.js

---
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -0,0 +1,9 @@
+showDebug = true; 
+showMessage = true; 
+var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
+var inspType = inspObj.getInspectionType();
+logDebug("Inspection Date= " + inspDate);
+
+//generate and attach the 5001 Inspection Report to the inspected record
+runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
+
