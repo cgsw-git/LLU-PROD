9d58141b14da75a0aa5b4f8d4a0297e9422b9d06
lluaca <44002415+lluaca@users.noreply.github.com>
2018-12-18 19:51:10 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -6,4 +6,11 @@ var capId = capId.getCapID();
 logDebug("Inspection Date= " + inspDate);
 
 //generate and attach the 5001 Inspection Report to the inspected record
-runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
+var myResult = aa.cap.getCap(inspObj.getCapID());
+if (myResult.getSuccess()){
+  cap = myResult.getOutput();
+  capId = cap.getCapID();
+  runReportAttach(capId,"5001 Inspection Report","departmentID",capId.getCustomID(),"inspectionDate",inspDate,"inspectionType",inspType);
+} else {
+  logDebug("ERROR: Could not get CapId : " + myResult.getErrorType() + " : " + myResult.getErrorMessage() );
+}
