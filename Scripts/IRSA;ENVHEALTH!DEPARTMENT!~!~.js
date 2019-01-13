bc7c2b968290d81fc742842d1590e945c9da91b9
lluaca <44002415+lluaca@users.noreply.github.com>
2019-01-13 15:51:25 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -1,22 +1,12 @@
-showDebug = true; 
-showMessage = true; 
+// Generate and attach the 5001 Inspection Report to the inspected record
+inspObj = aa.inspection.getInspection(capId,inspId).getOutput();
 var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
 var inspType = inspObj.getInspectionType();
-logDebug("Inspection Date= " + inspDate);
-
-//generate and attach the 5001 Inspection Report to the inspected record
-var myResult = aa.cap.getCap(inspObj.getCapID());
-if (myResult.getSuccess()){
-  var cap = myResult.getOutput();
-  var capId = cap.getCapID();
-  var customID = capId.getCustomID();
-  var reportParams = aa.util.newHashtable();
-  addParameter(reportParams,"departmentID",customID);
-  addParameter(reportParams, "inspectionDate",inspDate);
-  addParameter(reportParams, "inspectionType",inspType);
-  runReportAttach(capId,"5001 Inspection Report",reportParams);
-} else {
-  logDebug("ERROR: Could not get CapId : " + myResult.getErrorType() + " : " + myResult.getErrorMessage() );
-}
+var customID = capId.getCustomID();
+var reportParams = aa.util.newHashtable();
+addParameter(reportParams,"departmentID",customID);
+addParameter(reportParams, "inspectionDate",inspDate);
+addParameter(reportParams, "inspectionType",inspType);
+var myResult = runReportAttach(capId,"5001 Inspection Report",reportParams);
 // End generate and attach the 5001 Inspection Report to the inspected record
 
