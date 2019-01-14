fa0f579988febca7a0778cd020824176eb4b8089
lluaca <44002415+lluaca@users.noreply.github.com>
2019-01-13 16:58:43 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -3,7 +3,7 @@ inspObj = aa.inspection.getInspection(capId,inspId).getOutput();
 var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
 var inspType = inspObj.getInspectionType();
 var customID = capId.getCustomID();
-var reportParams = aa.util.newHashtable();
+var reportParams = aa.util.newHashMap();
 addParameter(reportParams,"departmentID",customID);
 addParameter(reportParams, "inspectionDate",inspDate);
 addParameter(reportParams, "inspectionType",inspType);
