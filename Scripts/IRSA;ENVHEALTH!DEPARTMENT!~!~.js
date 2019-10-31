410a3e7e9c4aae3aad3f68348507b410897275cd
lluaca <44002415+lluaca@users.noreply.github.com>
2019-10-31 16:14:51 -0700

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js
    Added parameters for 5001WithPctures.rpt inspection report.

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -1,4 +1,5 @@
 // Generate and attach the 5001 Inspection Report to the inspected record
+
 inspObj = aa.inspection.getInspection(capId,inspId).getOutput();
 var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
 var inspType = inspObj.getInspectionType();
@@ -7,6 +8,12 @@ var reportParams = aa.util.newHashMap();
 addParameter(reportParams,"departmentID",customID);
 addParameter(reportParams, "inspectionDate",inspDate);
 addParameter(reportParams, "inspectionType",inspType);
+
+// support for new 5001 inspection report with only inspection id parameter
+addParameter(reportParams,"g6ActNum",inspId);
+
+// generate report
 var myResult = runReportAttach(capId,"5001 Inspection Report",reportParams);
+
 // End generate and attach the 5001 Inspection Report to the inspected record
 
